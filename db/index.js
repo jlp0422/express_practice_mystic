97bab72d521c2d7ba90d87f172cde17695154262
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-01-27 15:51:49 -0500

    adding image

--- db/index.js
+++ db/index.js
@@ -1,13 +1,17 @@
 /* eslint-disable */
 
 const friends = [
-  {id: 1, firstName: 'Jeremy', lastName: 'Philipson', food: 'Pizza', accessory: 'iPad'},
-  {id: 2, firstName: 'Carolyn', lastName: 'Fine', food: 'Veggie nuggets', accessory: 'Headband'},
+  {id: 1, firstName: 'Jeremy', lastName: 'Philipson', food: 'Pancakes', accessory: 'iPad'},
+  {id: 2, firstName: 'Carolyn', lastName: 'Fine', food: 'Macaron', accessory: 'Headband'},
   {id: 3, firstName: 'Andrew', lastName: 'Greif', food: 'Pretzels', accessory: 'Music'},
   {id: 4, firstName: 'Elizabeth', lastName: 'Lafontaine', food: 'Hummus and dips', accessory: 'Earrings'},
   {id: 5, firstName: 'Dave', lastName: 'Ehrlich', food: 'Rugelach', accessory: 'Camera'},
   {id: 6, firstName: 'Alex', lastName: 'Horowitch', food: 'Watermelon', accessory: 'Sunglasses'},
-  {id: 7, firstName: 'Austin', lastName: 'Blau', food: 'Chicken fingers', accessory: 'Computer'}
+  {id: 7, firstName: 'Austin', lastName: 'Blau', food: 'Chicken fingers', accessory: 'Computer'},
+  {id: 8, firstName: 'Zach', lastName: 'Goldberg', food: 'Chicken wings', accessory: 'Apple Watch'},
+  {id: 9, firstName: 'Jillian', lastName: 'Ellbaum', food: 'Wonton spring rolls', accessory: 'Hiking boots'},
+  {id: 10, firstName: 'Jared', lastName: 'Bolnick', food: 'Bacon', accessory: 'Grill'},
+  {id: 11, firstName: 'Elena', lastName: 'Latzen', food: 'Gluten free flatbread', accessory: 'Baseball hat'}
 ]
 
 const getFriends = () => {
